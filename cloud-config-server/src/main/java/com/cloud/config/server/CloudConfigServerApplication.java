18a2e9725316abdc700dfd330b74de3f6a8d5042
Dharmender Rawat <dsrawat321@gmail.com>
2020-10-16 01:34:35 +0530

    Make the config server as eureka client

--- cloud-config-server/src/main/java/com/cloud/config/server/CloudConfigServerApplication.java
+++ cloud-config-server/src/main/java/com/cloud/config/server/CloudConfigServerApplication.java
@@ -3,10 +3,11 @@ package com.cloud.config.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 @EnableConfigServer
 public class CloudConfigServerApplication {
 
